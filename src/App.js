cd3c34839525987108db9a46bc721180a6ca3884
Jelena de la Sauce <jelena_de_la_sauce@web.de>
2022-02-09 14:11:50 +0100

    Add Header and modify rooms JS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,60 @@
 import './App.css';
+import Header from './Header.js';
 import Room from './Room.js';
 
 export default function App() {
+  const rooms = [
+    {
+      text: 'Küche',
+      description: 'Boden, Spüle',
+      isDescriptionVisible: true,
+      isClean: false,
+    },
+    {
+      text: 'Wohnzimmer',
+      description: 'Teppich, Heizung',
+      isDescriptionVisible: false,
+      isClean: true,
+    },
+    {
+      text: 'Bad',
+      description: 'Waschbecken, Spiegel ',
+      isDescriptionVisible: false,
+      isClean: false,
+    },
+  ];
+
   return (
     <main className="App">
-      <Room text="Küche" isClean />
-      <Room text="Wohnzimmer" isClean={true} />
-      <Room text="Bad" isClean={false} />
+      <Header>Happy Cleaning!</Header>
+      {/* landet so direkt in children und wird darüber dahingeschoben // Header in Main, da dieser sich auf jeder Seite ändern soll (Wenn gewünscht, div ganz außen, dann header und die Rooms in main*/}
+      {/* fest eingegeben, nicht variabel als Array
+       <Room
+        text="Küche"
+        description="Boden, Spüle"
+        isDescriptionVisible
+        isClean={false}
+      />
+     // könnte man auch mit Children machen, aber da noch extra Text lieber über Attribut 
+      <Room
+        text="Wohnzimmer"
+        description="Teppich, Fensterbank"
+        isDescriptionVisible
+        isClean={true}
+      />
+      <Room text="Bad" description="Dusche, Waschbecken" isClean={false} /> */}
+
+      {/* Es könnten auch die keys als Parameter eingetragen werden, sodass unden das rooms bei rooms.text weggelassen werden kann  */}
+
+      {rooms.map((room) => (
+        <Room
+          key={room.text}
+          text={room.text}
+          description={room.description}
+          isDescriptionVisible={room.isDescriptionVisible}
+          isClean={room.isClean}
+        />
+      ))}
     </main>
   );
 }
--- src/Room.js
+++ src/Room.js
@@ -1,9 +1,20 @@
 import './Room.css';
 
-export default function Room({ text, isClean }) {
+export default function Room({
+  text,
+  description,
+  isDescriptionVisible,
+  isClean,
+}) {
+  const statusClassName = isClean
+    ? `Room__status Room__status--clean`
+    : `Room__status Room__status--dirty`; // `Room__status Room__status--${isClean ? 'clean' : 'dirty'}`
   return (
     <section className="Room">
-      {text} {isClean ? 'yes' : 'no'}
+      {text}
+      <div className={statusClassName}></div>
+      <p hidden={!isDescriptionVisible}>{description}</p>
+      {/* alternativ: {isDescriptionVisible && <p>{description}</p>} */}
     </section>
   );
 }
